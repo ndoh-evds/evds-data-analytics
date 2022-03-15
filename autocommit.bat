104ba322047fdf774702abbf7904fe439410820e
Sboniso Masilela <u10416260@tuks.co.za>
2022-03-15 16:18:00 +0200

    uploaded public export files

--- autocommit.bat
+++ autocommit.bat
@@ -1,3 +1,3 @@
-git add -u                            
-git commit -m "uploaded public export files"      
-git push origin 2021.12.12                   
+git add --all                       
+git commit -m "uploaded public export files"
+git push origin 2021.12.12 
